f08a58e92d096dc9ee430de13834d88f44d33848
Arjumandwani <106468375+Arjumandwani@users.noreply.github.com>
2022-06-15 12:43:27 +0530

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{int a,b,sum;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("the addition of %d and %d is %d",a,b,sum);
+return 0;
+}
