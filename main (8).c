fdbc62f476ec1f06ac986165f39ec94c42266f7f
Arjumandwani <106468375+Arjumandwani@users.noreply.github.com>
2022-06-15 13:02:17 +0530

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{int i;
+for(i=100;i>=1;i--)
+{
+    printf("%d ",i);
+}
+
+    return 0;
+}
