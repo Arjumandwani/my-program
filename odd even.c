4046ca2c69a854bc744125508642f489abcc7d1c
Arjumandwani <106468375+Arjumandwani@users.noreply.github.com>
2022-06-15 12:47:41 +0530

    Add files via upload

---
+++ odd even.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{int n;
+printf("please enter a number");
+scanf("%d",&n);
+if("n%2==0")
+{
+printf("the number is even");
+}
+else 
+{printf("The number is odd");
+}
+return 0;
+}
